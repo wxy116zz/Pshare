8059ec91fcfc3c877063051b491ceb8d27d7dedb
Jun ShangGuan <wxy116zz@users.noreply.github.com>
2015-11-10 20:56:09 -0500

    Create department.php

---
+++ department.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=gb18030">
+<title>Pshare</title>
+<style type="text/css">
+<!--
+.STYLE10 {font-family: "Times New Roman", Times, serif}
+.STYLE13 {font-size: 36px}
+.STYLE16 {font-size: 18px}
+-->
+</style>
+</head>
+<body>
+<table width="1191" border="0">
+  <tr>
+    <td>&nbsp;</td>
+    <td width="66" align="center"><span class="STYLE10"><a href="department.php">Catalog</a></span></td>
+  </tr>
+</table>
+<table width="1191" border="0">
+  <tr>
+    <td width="224" height="94"><a href="index.php"><img src="image/Pshare logo.png" alt="1" width="224" height="90" border="0" /></a></td>
+    <th width="951"> <span class="STYLE13">Course and Textbook</span></th>
+  </tr>
+</table>
+<table width="1191" border="0">
+  <tr>
+    <td width="46"><div align="center"><a href="catalog.php?id=A">A</a></div></td>
+    <td width="46"><div align="center"><a href="catalog.php?id=B">B</a></div></td>
+    <td width="46"><div align="center"><a href="catalog.php?id=C">C</a></div></td>
+    <td width="46"><div align="center"><a href="catalog.php?id=D">D</a></div></td>
+    <td width="46"><div align="center"><a href="catalog.php?id=E">E</a></div></td>
+    <td width="46"><div align="center"><a href="catalog.php?id=F">F</a></div></td>
+    <td width="46"><div align="center"><a href="catalog.php?id=G">G</a></div></td>
+    <td width="46"><div align="center"><a href="catalog.php?id=H">H</a></div></td>
+    <td width="46"><div align="center"><a href="catalog.php?id=I">I</a></div></td>
+    <td width="46"><div align="center"><a href="catalog.php?id=J">J</a></div></td>
+    <td width="46"><div align="center"><a href="catalog.php?id=K">K</a></div></td>
+    <td width="46"><div align="center"><a href="catalog.php?id=L">L</a></div></td>
+    <td width="46"><div align="center"><a href="catalog.php?id=M">M</a></div></td>
+    <td width="46"><div align="center"><a href="catalog.php?id=N">N</a></div></td>
+    <td width="46"><div align="center"><a href="catalog.php?id=O">O</a></div></td>
+    <td width="46"><div align="center"><a href="catalog.php?id=P">P</a></div></td>
+    <td width="46"><div align="center"><a href="catalog.php?id=Q">Q</a></div></td>
+    <td width="46"><div align="center"><a href="catalog.php?id=R">R</a></div></td>
+    <td width="46"><div align="center"><a href="catalog.php?id=S">S</a></div></td>
+    <td width="46"><div align="center"><a href="catalog.php?id=T">T</a></div></td>
+    <td width="46"><div align="center"><a href="catalog.php?id=U">U</a></div></td>
+    <td width="46"><div align="center"><a href="catalog.php?id=V">V</a></div></td>
+    <td width="46"><div align="center"><a href="catalog.php?id=W">W</a></div></td>
+    <td width="46"><div align="center"><a href="catalog.php?id=X">X</a></div></td>
+    <td width="46"><div align="center"><a href="catalog.php?id=Y">Y</a></div></td>
+    <td width="46"><div align="center"><a href="catalog.php?id=Z">Z</a></div></td>  
+  </tr>
+</table>
+<hr align="left" width="1200" />
+<br>
+<table width="1199" height="191" border="0">
+  <tr>
+    <td align="center"><span class="STYLE16"><a href="department-catalog.php?dp=BME">Biomedical Engineering</a></span></td>
+    <td align="center"><span class="STYLE16"><a href="department-catalog.php?dp=EE">Electrical Engineering</a></span></td>
+    <td align="center"><span class="STYLE16"><a href="department-catalog.php?dp=BIA">Business Intelligence and Analytics</a></span></td>
+    <td align="center"><span class="STYLE16"><a href="department-catalog.php?dp=CH">Chemistry</a></span></td>
+  </tr>
+  <tr>
+    <td align="center"><span class="STYLE16"><a href="department-catalog.php?dp=FE">Financial Engineering</a></span></td>
+    <td align="center"><span class="STYLE16"><a href="department-catalog.php?dp=CS">Computer Science</a></span></td>
+    <td align="center"><span class="STYLE16"><a href="department-catalog.php?dp=CPE">Computer Engineering</a></span></td>
+    <td align="center"><span class="STYLE16"><a href="department-catalog.php?dp=MIS">Information System</a></span></td>
+  </tr>
+  <tr>
+    <td align="center"><span class="STYLE16"><a href="department-catalog.php?dp=CHE">Chemical Engineering</a></span></td>
+    <td align="center"><span class="STYLE16"><a href="department-catalog.php?dp=CE">Civil Engineering</a></span></td>
+    <td align="center"><span class="STYLE16"><a href="department-catalog.php?dp=MT">Material Engineering</a></span></td>
+    <td align="center"><span class="STYLE16"><a href="department-catalog.php?dp=ME">Mechanical Engineering</a></span></td>
+  </tr>
+  <tr>
+    <td align="center"><span class="STYLE16"><a href="department-catalog.php?dp=BT">Business and Technology</a></span></td>
+    <td align="center"><span class="STYLE16"><a href="department-catalog.php?dp=CM">Construction Management</a></span></td>
+    <td align="center"><span class="STYLE16"><a href="department-catalog.php?dp=EN">Environmental Engineering</a></span></td>
+    <td align="center"><span class="STYLE16"><a href="department-catalog.php?dp=ES">Enterprise System</a></span></td>
+  </tr>
+  <tr>
+    <td align="center"><span class="STYLE16"><a href="department-catalog.php?dp=H">Humanities</a></span></td>
+    <td align="center"><span class="STYLE16"><a href="department-catalog.php?dp=OE">Ocean Engineering</a></span></td>
+    <td align="center"><span class="STYLE16"><a href="department-catalog.php?dp=PME">Pharmaceutical Manufacturing</a></span></td>
+    <td align="center"><span class="STYLE16"><a href="department-catalog.php?dp=PE">Physical Education</a></span></td>
+  </tr>
+  <tr>
+    <td align="center"><span class="STYLE16"><a href="department-catalog.php?dp=MA">Mathematics</a></span></td>
+    <td align="center"><span class="STYLE16"><a href="department-catalog.php?dp=EM">Engineering Management</a></span></td>
+    <td align="center"><span class="STYLE16"><a href="department-catalog.php?dp=L">Language</a></span></td>
+    <td align="center"><span class="STYLE16"><a href="department-catalog.php?dp=O">Other</a></span></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+</body>
+</html>
